36c2b495c6173d34ff53f5cd931d698a9bfb3714
Claudio Semeraro <raptxt@gmail.com>
2011-05-05 23:35:44 +0200

    Cleanup

--- config.rb
+++ config.rb
@@ -11,17 +11,15 @@ require 'cgi'
 require File.join( File.expand_path(File.dirname(__FILE__)), 'lib/extend_string.rb' )
 
 # Database connection.
-#DB = Sequel.connect 'sqlite://raptxt.db'
 DB = Sequel.mysql 'raptxt', :user => 'root', :password => '', :host => '127.0.0.1', :socket => '/Applications/MEPP/tmp/mysql/mysql.sock', :logger => Logger.new('tmp/db.log')
 
-# Sequel schema plugin.
+# Sequel schema plugins.
 Sequel::Model.plugin :schema
 Sequel::Model.plugin :force_encoding, 'UTF-8'
 Sequel::MySQL.convert_invalid_date_time = nil
 
 # Database models.
 Dir['models/*.rb'].each { |model| require File.join( File.expand_path(File.dirname(__FILE__)), model ) }
-
 
 # Blog configurations.
 Settings = Struct.new(:title, :desc, :code, :feed)
